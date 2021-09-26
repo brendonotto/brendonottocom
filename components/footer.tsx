147f2b68d69c58ae1ce4d14a54223f9380f5a5f1
Brendon Otto <brendon@brendonotto.com>
2021-09-26 11:27:34 -0500

    Added link to RSS feed and threw in social links too, closes #2

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,14 @@ const Footer = () => {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <span>© Copyright 2021 Brendon Otto</span>
+        <div className="py-24 flex flex-col items-center space-y-1">
+          <a href="/feed.xml">RSS</a>
+          <a href="https://twitter.com/brendondotto">Twitter</a>
+          <a href="https://github.com/brendonotto">Github</a>
+          <a href="https://youtube.com/brendonotto">YouTube</a>
+          <div>
+            <span>©2021 Brendon Otto</span>
+          </div>
         </div>
       </Container>
     </footer>
